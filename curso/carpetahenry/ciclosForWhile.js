34f1af63b564d78f6e9fbd4379254bca2b59c3a0
Sebastian Ortiz <mugssebastian@gmail.com>
2023-01-21 00:27:50 -0500

    apuntes de la clase

---
+++ curso/carpetahenry/ciclosForWhile.js
@@ -0,0 +1,15 @@
+ var suma = 0;
+
+for (let i = 0; i < 5; i++) {
+    suma = suma + i;
+    console.log('Variable de iteración: ', i);
+ }
+ console.log('Variable suma: ', suma);
+
+
+ var suma = 0;
+
+ while(suma < 3) {
+    suma = suma + 1;
+        console.log(suma);
+    };
