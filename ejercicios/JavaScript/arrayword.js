eb54d9cef07929aa29ea3526361bf35a28455fbf
Sebastian Ortiz <mugssebastian@gmail.com>
2023-01-26 23:44:33 -0500

    apuntes y board

--- ejercicios/JavaScript/arrayword.js
+++ ejercicios/JavaScript/arrayword.js
@@ -9,3 +9,35 @@ function encontrandoLetra (string){
 
 encontrandoLetra("sebastian come pan")
 encontrandoLetra("pedro")
+
+var num = 956;
+var str = num.toString();
+var array = str.split('')
+console.log(array)
+for(var i = 0; i < array.length; i++){
+    if(array[i] === '9'){
+        console.log (true);
+    }
+}
+console.log(false)
+
+var a = [1, 2, 3, 4, 9, 6, 7, 8, 5]
+for (let i = 0; i < a.length; i++) {
+    if (a[i] === 9) {
+        console.log("lo encontre")
+        console.log("y me detuve")
+      break ; 
+    }if(a[i] === 8)
+        console.log("no sali")
+  }
+  
+    let i = 0;
+    let n = 0;
+    while (i < 5) {
+    i++;
+    if (i === 3) {
+     continue;
+    }
+    n += i;
+    console.log(n);
+}
