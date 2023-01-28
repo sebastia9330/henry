14ced3205b946240d009ac2b59f7fa9c65d88e58
Sebastian Ortiz <mugssebastian@gmail.com>
2023-01-27 23:38:58 -0500

    class notes 27/01/23

---
+++ curso/carpetahenry/metodosobjects.js
@@ -0,0 +1,28 @@
+// HAS OWN PROPERTY busca la propiedad indicada
+var libro = { autor: 'Borges', genero: 'Policial', año: 1990 };
+var tienePropiedad = libro.hasOwnProperty('nombre');
+
+console.log(tienePropiedad);
+
+// KEYS imprime solo las llaves o claves del objeto
+var libro = { autor: 'Borges', genero: 'Policial', año: 1990 };
+var todasLasPropiedades = Object.keys(libro);
+
+console.log(todasLasPropiedades);
+
+//ciclo foi-in
+for (let prop in mundo) {
+    console.log('Esta es la propiedad: ', prop);
+    console.log('Este es el valor: ', mundo[prop]);
+ }
+
+ //THIS
+var mascota = {
+    animal: 'Perro',
+    raza: 'Ovejero Alemán',
+    amistoso: true,
+    dueño: 'María López',
+    info: () => {
+       console.log('Mi perro es un  ' + this.raza);
+    },
+ };
---
+++ curso/carpetahenry/objects.js
@@ -0,0 +1,40 @@
+var deportes = {
+    conBalon: ['Futbol', 'Basketball', 'Golf'],
+    sinBalon: ['Boxeo', 'Surf', 'Trekking'],
+ };
+ var persona = { nombre: 'Lucas', edad: 26, estudios: { esProgramador: true } };
+ console.log(persona)
+
+ // ACCEDER
+var persona = { nombre: 'Lucas', edad: 26, estudios: { esProgramador: true } };
+console.log(persona.edad);
+
+// ASIGNAR
+var persona = { nombre: 'Lucas', edad: 26, estudios: { esProgramador: true } };
+persona.nombre = 'Martín';
+console.log(persona.nombre);
+
+//CREAR
+var autos = {};
+autos.marcas = ['Ford', 'Audi', 'Ferrari'];
+console.log(autos);
+
+//BORRAR
+delete autos.marcas;
+console.log(autos);
+
+// DOT NOTATION
+var atuendos = { manos: ['Guantes', 'Anillos'], pies: ['Zapatos', 'Soquetes'] };
+
+// BRACKET NOTATION
+atuendos['piernas'] = ['Bermudas', 'Pantalones'];
+console.log(atuendos);
+
+
+//funcion dentro de un objeto con variables externas
+var comidas = {};
+var diferenciaDeNotaciones = function(propUno, propDos){
+    comidas,propUno = ['frutas', 'verduras'];
+    comidad[propDos] = ['hamburguesa', 'pizza'];
+};
+
