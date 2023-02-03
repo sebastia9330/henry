b845b80b80b17237f5ed395d467a9dbdb18dd29a
Sebastian Ortiz <mugssebastian@gmail.com>
2023-02-03 00:18:09 -0500

    conteodeletras

--- ejercicios/JavaScript/conteodeletras.js
+++ ejercicios/JavaScript/conteodeletras.js
@@ -1,10 +1,23 @@
-var letras = "adsjfdsfsfjsdjfhacabcsbajda";
+var letras = "kkkkkkkkkkkkkkkkkkkkkkk";
 var nuevoArray = letras.split('');
-var contador = 0;
+var contador = 1;
+nuevoArray.sort()
+var unicos = [];
+var contadorRep = [];
+var objt = {}
+console.log(nuevoArray)
 for (var i = 0; i < nuevoArray.length; i++){
-    if(nuevoArray[i] === nuevoArray[i+1]){
-        contador = contador + 1
+    if(nuevoArray[i+1] === nuevoArray[i]){
+        contador ++;
+    }else{
+        unicos.push(nuevoArray[i]);
+        contadorRep.push(contador);
+        contador = 1;
     }
-    
 }
-console.log(contador)
+for(var i = 0; i < unicos.length; i++){
+    for(var j = 0; j<contadorRep.length; j++){
+       objt[unicos[i]] = [contadorRep[j]]
+    }
+ }
+console.log(objt)
